1b8dc9b62eee6b2197a444cf2cdaad6bddb0672f
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-15 18:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const date11 = new Date(date1);
+	const date22 = new Date(date2);
+	let milliSec1 = date11.getTime();
+	let milliSec2 = date22.getTime();
+
+	let diff = Math.abs(milliSec2 - milliSec1);
+	return Math.ceil(diff / (1000*3600*24));
 };
 
 // Do not change the code below.
