6cb6fcddb5a2e7288ade7e317a666d41f20012d2
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-16 18:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var dateDiffInDays = function (date1, date2) {
 	let milliSec1 = date11.getTime();
 	let milliSec2 = date22.getTime();
 
-	let diff = Math.abs(milliSec2 - milliSec1);
+	let diff = (milliSec2 - milliSec1);
 	return Math.ceil(diff / (1000*3600*24));
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+alert(dateDiffInDays(dateOne, dateTwo)); 
